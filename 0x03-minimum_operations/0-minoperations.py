9b44399c76b9fae0e80449f2598b65057122d817
Sean Taylor <silverwolf4350@gmail.com>
2021-01-07 14:35:45 -0600

    Reverted better limit fix

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -6,10 +6,7 @@ def minOperations(n):
     if n < 1:
         return 0
 
-    res = operate(n, 1, n, 1, 1)
-    if res != n and n > 50:
-        res -= 1
-    return res
+    return operate(n, 1, n, 1, 1)
 
 def operate(n, opps, lowest, copied, total):
     """ Recursively checks every possible path """
@@ -20,9 +17,6 @@ def operate(n, opps, lowest, copied, total):
 
     if total > n:
         return lowest
-
-    if n > 50 and opps > (n / 3 * 2):
-        return n
 
     lowest = operate(n, opps + 1, lowest, copied, total + copied)
 
