82ffdd4840558b4cd21b4075027a796706de6c45
Sean Taylor <silverwolf4350@gmail.com>
2021-01-22 04:22:56 +0000

    Created all files for mandatory tasks.

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a linked list is a palindrome
+ * @head: list to check
+ *
+ * Return: 1 if palindrome, 0 otherwise
+ */
+int is_palindrome(listint_t **head)
+{
+	int len;
+	listint_t *p = *head;
+
+	for (len = 0; p != NULL; len++)
+		p = p->next;
+
+	return (check(*head, len - 1));
+}
+
+/**
+ * check - checks if a list is a palindrome
+ * @s: list to check
+ * @x: start of the list
+ *
+ * Return: 1 if palindrome, 0 otherwise
+ */
+int check(listint_t *s, int x)
+{
+	int i;
+	listint_t *j = s;
+
+	for (i = 0; i < x; i++)
+		 j = j->next;
+
+	if (s->n != j->n)
+		return (0);
+	if (x <= 0)
+		return (1);
+	return (check(s->next, x - 2));
+}
