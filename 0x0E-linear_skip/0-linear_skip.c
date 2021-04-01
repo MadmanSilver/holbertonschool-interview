3cda6822222d61babcd1d4a9865f91f049aa412b
Sean Taylor <silverwolf4350@gmail.com>
2021-04-01 19:37:58 +0000

    Created all files for mandatory tasks.

---
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -0,0 +1,53 @@
+#include "search.h"
+
+/**
+ * linear_skip - searches for a value in a sorted skip list
+ * @list: list to search through
+ * @value: value to search for
+ *
+ * Return: node where value is located, NULL otherwise
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *lo = list;
+	skiplist_t *hi;
+
+	if (list == NULL)
+		return (NULL);
+
+	if (list->n > value)
+		return (NULL);
+
+	hi = lo->express;
+
+	while (hi != NULL && hi->n < value)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", hi->index, hi->n);
+		lo = hi;
+		hi = hi->express;
+	}
+
+	if (hi == NULL)
+	{
+		hi = lo->next;
+		while (hi->next != NULL)
+			hi = hi->next;
+	}
+
+	if (hi->express != NULL)
+		printf("Value checked at index [%lu] = [%d]\n", hi->index, hi->n);
+
+	printf("Value found between indexes [%lu] and [%lu]\n", lo->index, hi->index);
+
+	while (lo != NULL && lo->n < value)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", lo->index, lo->n);
+		lo = lo->next;
+	}
+
+	if (lo == NULL)
+		return (NULL);
+
+	printf("Value checked at index [%lu] = [%d]\n", lo->index, lo->n);
+	return (lo);
+}
---
+++ 0x0E-linear_skip/README.md
@@ -0,0 +1,1 @@
+0x0E Linear Skip is a coding challenge intended as interview prep.
