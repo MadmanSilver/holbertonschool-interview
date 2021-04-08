86be236f6715b6893590d80fe1c7936007ed9806
Sean Taylor <silverwolf4350@gmail.com>
2021-04-08 00:03:11 +0000

    Made the character retrieval async.

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -2,16 +2,25 @@
 
 const request = require('request');
 
+const asyncChars = async function (url) {
+  return new Promise(function (resolve, reject) {
+    request(url, function (err, res, body) {
+      if (err) {
+        reject(err);
+      }
+      resolve(JSON.parse(body).name);
+    });
+  });
+};
+
 request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
   if (err) {
     return console.log(err);
   }
-  JSON.parse(body).characters.forEach(character => {
-    request(character, function (err, res, body) {
-      if (err) {
-        return console.log(err);
-      }
-      console.log(JSON.parse(body).name);
+  (async function () {
+    JSON.parse(body).characters.forEach(character => {
+      const res = await asyncChars(character);
+      console.log(res);
     });
-  });
+  })();
 });
