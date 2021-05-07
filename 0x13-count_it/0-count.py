b7a93a77a27c377e0d4d0c419c1af1bf5b3e727c
Sean Taylor <silverwolf4350@gmail.com>
2021-05-06 19:31:26 -0500

    Major refactor of count_words function.

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -3,40 +3,28 @@ import requests
 import operator
 
 
-def count_words(subreddit, word_list, after=None, keyword=None):
+def count_words(subreddit, word_list, after=None):
     """ Prints a sorted count of given keywords from the titles of hot articles on a subreddit. """
-    if after is None:
-        hot = requests.get('https://api.reddit.com/r/{}/hot'.format(subreddit),
-                           headers={'User-Agent': 'madmansilver'},
-                           allow_redirects=False).json()
-    else:
-        hot = requests.get('https://api.reddit.com/r/{}/hot?after={}\
-'.format(subreddit, after),
-                           headers={'User-Agent': 'madmansilver'},
-                           allow_redirects=False).json()
+    if word_list is None:
+        print()
+        return
+
+    if isinstance(word_list, list):
+        word_list = {key: 0 for key in word_list}
+
+    hot = requests.get(('https://api.reddit.com/r/{}/hot' + '?after={}' if after is not None else '').format(subreddit, after),
+                        headers={'User-Agent': 'madmansilver'}, allow_redirects=False).json()
 
     if 'data' not in hot:
-        return None
+        for item in sorted(res.items(), key=operator.itemgetter(1), reverse=True):
+            if (item[1] != 0):
+                print('{}: {}'.format(item[0], item[1]))
+        
+        return
 
-    if keyword is not None:
-        count = 0
-        for post in hot.get('data').get('children'):
-            if keyword.lower() in post.get('data').get('title').lower():
-                for word in post.get('data').get('title').split():
-                    if word.lower() == keyword.lower():
-                        count += 1
+    for post in hot.get('data').get('children'):
+        for key in word_list.keys():
+            word_list[key] += post.get('data').get('title').lower().count(key.lower())
 
-        after = hot.get('data').get('after')
-
-    if after is not None:
-        return count_words(subreddit, None, after) + count
-    if word_list is None and after is None:
-        return count
-
-    res = {}
-    for word in word_list:
-        res[word] = count_words(subreddit, None, None, word)
-
-    for item in sorted(res.items(), key=operator.itemgetter(1), reverse=True):
-        if (item[1] != 0):
-            print('{}: {}'.format(item[0], item[1]))
+    after = hot.get('data').get('after')
+    count_words(subreddit, word_list, after)
