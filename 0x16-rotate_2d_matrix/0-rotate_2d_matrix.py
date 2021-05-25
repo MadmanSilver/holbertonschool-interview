332a607bc1995f8dd23d4370513c7c8a63216a34
Sean Taylor <silverwolf4350@gmail.com>
2021-05-25 11:46:21 -0500

    Created all files for mandatory tasks.

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Contains the rotateMatrix function. """
+
+def rotate_2d_matrix(matrix):
+    """ Rotates a 2D matrix 90 degrees clockwise. """
+    size = len(matrix)
+
+    for i in range(3):
+        for x in range(0, int(size / 2)):
+            for y in range(x, size - x - 1):
+                tmp = matrix[x][y]
+                matrix[x][y] = matrix[y][size - 1 - x]
+                matrix[y][size - 1 - x] = matrix[size - 1 - x][size - 1 - y]
+                matrix[size - 1 - x][size - 1 - y] = matrix[size - 1 - y][x]
+                matrix[size - 1 - y][x] = tmp
---
+++ 0x16-rotate_2d_matrix/README.md
@@ -0,0 +1,1 @@
+0x16 Rotate 2D Matrix is a coding challenge intended as interview prep.
