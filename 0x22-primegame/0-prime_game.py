6c96045f10ef3ce8f28d04727e3f2be713beaa69
Sean Taylor <silverwolf4350@gmail.com>
2021-08-25 18:03:46 -0500

    Significantly reduced runtime on larger sets.

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """Contains the isWinner function."""
 
+
 def isWinner(x, nums):
     """Calculates the winner of the Prime Game."""
     mariaW = 0
     benW = 0
-    
+    pnums = {2, 3, 5, 7}
+    largestp = 7
+
     if type(nums) is not list or x > len(nums):
         return None
 
@@ -13,10 +16,19 @@ def isWinner(x, nums):
         primes = 0
 
         for num in range(2, nums[i] + 1):
+            if num in pnums:
+                primes += 1
+                continue
+            if num < largestp:
+                continue
+
             for p in range(2, int(num/2) + 1):
                 if num % p == 0:
                     break
             else:
+                pnums.add(num)
+                if num > largestp:
+                    largestp = num
                 primes += 1
 
         if primes % 2 == 1:
